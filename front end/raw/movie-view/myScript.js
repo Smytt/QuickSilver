3374337d950dda4049d7a78455940e7e0f3f6b86
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2018-07-27 18:33:34 +0300

    'front-end'

---
+++ front end/raw/movie-view/myScript.js
@@ -0,0 +1,28 @@
+// var movieCircle = document.createElement("DIV");
+// movieCircle.style.borderRadius = "50%";
+// movieCircle.style.width = "300px";
+// movieCircle.style.height = "300px";
+// movieCircle.style.marginLeft = "20px";
+// movieCircle.style.backgroundImage = "url('matrix.jpg')";
+// movieCircle.style.backgroundSize = "300px";
+
+// movieCircle.style.display = "inline-block";
+// document.getElementById("poster").appendChild(movieCircle);
+
+// var movieTitle = document.createElement("DIV");
+// var t = document.createTextNode("The Matrix");
+// movieTitle.appendChild(t);
+// movieTitle.style.display = "inline-block"
+// movieTitle.style.color = "#0c2c60";
+// movieTitle.style.fontSize = "30px";
+
+
+// var movieInfo = document.createElement("DIV");
+// t = document.createTextNode("A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.");
+// movieInfo.appendChild(t);
+// movieInfo.style.color = "#3e9cd6";
+// movieInfo.style.fontSize = "15px";
+
+// document.getElementById("movie-info").appendChild(movieTitle);
+
+// document.getElementById("movie-info").appendChild(movieInfo);
